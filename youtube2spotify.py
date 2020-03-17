4f6a3143cc6bcefdb650f731110102c8ed2f00f4
Yiannis <johngate10@hotmail.com>
2020-03-17 21:51:28 +0200

    oops

--- youtube2spotify.py
+++ youtube2spotify.py
@@ -29,7 +29,7 @@ AUTHORIZATION_URL = (
     "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent"
 )
 AUTHORIZATION_SCOPE = "https://www.googleapis.com/auth/youtube.readonly"
-BASE_URI = "http://youtube-2-spotify.herokuapp.com/"
+BASE_URI = "http://youtube-2-spotify.herokuapp.com"
 AUTH_REDIRECT_URI = BASE_URI + "/google/auth"
 
 
