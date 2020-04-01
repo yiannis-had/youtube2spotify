a882b6cddb31188059e7ffbdaa99579d60671115
Yiannis <johngate10@hotmail.com>
2020-04-01 16:22:16 +0300

    v2.0

--- youtube2spotify.py
+++ youtube2spotify.py
@@ -30,7 +30,7 @@ AUTHORIZATION_URL = (
     "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent"
 )
 AUTHORIZATION_SCOPE = "https://www.googleapis.com/auth/youtube.readonly"
-BASE_URI = "http://youtube-2-spotify.herokuapp.com/"
+BASE_URI = "http://youtube-2-spotify.herokuapp.com"
 
 DEVELOPER_KEY = "AIzaSyCrshTarqbNMy48VDdzeAoafBNOT8kZy5I"
 
